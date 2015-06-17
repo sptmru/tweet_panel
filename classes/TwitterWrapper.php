1b8613f89dc6955c179af3d59e68d5540adfec63
Soslan Aldatov <me@supporteam.ru>
2015-06-17 17:44:07 +0500

    getTimeline method

--- classes/TwitterWrapper.php
+++ classes/TwitterWrapper.php
@@ -7,6 +7,14 @@ class TwitterWrapper {
 	private $settings = array();
 	private $Twitter;
 
+	private function makeGETRequest($url, $getField) {
+		$requestMethod = 'GET';
+		$result = $this->Twitter->setGetfield($getField)
+			->buildOauth($url, $requestMethod)
+			->performRequest();
+		return $result;
+	}
+	
 	public function __construct($oauthToken, $oauthSecret, $consumerKey, $consumerSecret) {
 		$this->settings = array(
 			'oauth_access_token' => $oauthToken,
@@ -17,12 +25,15 @@ class TwitterWrapper {
 		$this->Twitter = new TwitterAPIExchange($this->settings);
 	}
 
-	public function makeGETRequest($url, $getField) {
-		$requestMethod = 'GET';
-		$result = $this->Twitter->setGetfield($getField)
-			->buildOauth($url, $requestMethod)
-			->performRequest();
+	public function getTimeline($username) {
+		$requestUrl = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+		$requestParams = '?screen_name='.$username;
+		$jsonResult = $this->makeGETRequest($requestUrl, $requestParams);
+		$result = json_decode($jsonResult);
+
 		return $result;
 	}
+
+
 
 }
