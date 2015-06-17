61cf74e4d83ea029e830e6cb1872011ef40088c7
Soslan Aldatov <me@supporteam.ru>
2015-06-17 18:18:50 +0500

    getAllTweetsFor fix

--- classes/TwitterWrapper.php
+++ classes/TwitterWrapper.php
@@ -25,9 +25,8 @@ class TwitterWrapper {
 		$this->Twitter = new TwitterAPIExchange($this->settings);
 	}
 
-	public function getTimelineFor($username, $requestParams = '?screen_name='.$username) {
+	public function getTimelineFor($username, $requestParams) {
 		$requestUrl = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
-		//$requestParams = '?screen_name='.$username;
 		$jsonResult = $this->makeGETRequest($requestUrl, $requestParams);
 		$result = json_decode($jsonResult);
 
