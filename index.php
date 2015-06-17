96d1e9059fa81a97f618755ee8226f30be6e7900
Soslan Aldatov <me@supporteam.ru>
2015-06-17 21:43:18 +0500

    Added simple design, menu

--- index.php
+++ index.php
@@ -3,6 +3,41 @@
 require_once ('classes/TwitterWrapper.php');
 $TwitterWrapper = new TwitterWrapper();
 
-echo "Sample Index Page";
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tweets</title>
+	<!-- Latest compiled and minified Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<!-- Optional Bootstrap theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+	<!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+<body>
+	<div class="site-wrapper">
+		<div class="site-wrapper-inner">
+			<div class="cover-container">
+				<div class="masthead clearfix">
+					<div class="inner">
+						<h3 class="masthead-brand">Tweets</h3>
+						<nav>
+							<ul class="nav masthead-nav">
+								<li class="active"><a href="#">Show tweets</a></li>
+								<li><a href="#">Load tweets to DB</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
