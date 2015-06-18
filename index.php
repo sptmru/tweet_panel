d6fa7334f461588fc6c64fa2e5d6a508fdf4668e
Soslan Aldatov <me@supporteam.ru>
2015-06-18 15:06:19 +0500

    design fix

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tweets</title>
+	<!-- Latest compiled and minified Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<!-- Optional Bootstrap theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+	<!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+</head>
+
 <?php 
 
 require_once ('classes/TwitterWrapper.php');
@@ -22,28 +39,12 @@ if(isset($_GET['action'])) {
 		$id = $_GET['id'];
 		$TwitterWrapper->answerTo($id, $tweet);
 		echo "<h1>Answered</h1>";
-		echo $tweet;
 	}
 }
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Tweets</title>
-	<!-- Latest compiled and minified Bootstrap CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
-	<!-- Optional Bootstrap theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-
-	<!-- Custom styles for this template -->
-    <link href="css/custom.css" rel="stylesheet">
-</head>
 <body>
 	<div class="site-wrapper">
 		<div class="site-wrapper-inner">
